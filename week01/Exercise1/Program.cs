7488ce24f56f369d3ca86b5d81e7e8bfedfef686
Nathan <21natwood@gmail.com>
2025-05-08 13:26:43 -0600

    Completed week01 projects 1 and 2

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -4,6 +4,13 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise1 Project.");
+        Console.WriteLine($"Please enter your first name: ");
+        string firstname = Console.ReadLine();
+
+        Console.WriteLine($"Please enter your last name: ");
+        string lastname = Console.ReadLine();
+
+        Console.WriteLine();
+        Console.WriteLine($"Your name is {lastname}, {firstname} {lastname}");
     }
 }
--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -1,9 +1,46 @@
 using System;
+using Microsoft.VisualBasic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        string letterGrade = "";
+
+        Console.WriteLine("What is your grade percentage? ");
+        string gradeString = Console.ReadLine();
+        float grade = float.Parse(gradeString);
+
+        if (grade >= 90)
+        {
+            letterGrade = "A";
+        }
+        else if (grade >= 80)
+        {
+            letterGrade = "B";
+        }
+        else if (grade >= 70)
+        {
+            letterGrade = "C";
+        }
+        else if (grade >= 60)
+        {
+            letterGrade = "D";
+        }
+        else if (grade < 60)
+        {
+            letterGrade = "F";
+        }
+
+        Console.WriteLine($"Your grade is {letterGrade}");
+
+        if (grade >= 70)
+        {
+            Console.WriteLine("You Pass!");
+        }
+        else if (grade < 70)
+        {
+            Console.WriteLine("You fail.");
+        }
     }
 }
