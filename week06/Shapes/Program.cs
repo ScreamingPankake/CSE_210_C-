bcb51d69ec63b5763a025c6994f14d7358aa89ab
Nathan <21natwood@gmail.com>
2025-06-09 11:17:47 -0600

    W06 Learning Activity: Polymorphism

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -4,6 +4,17 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Shapes Project.");
+
+        Square square = new Square("Red", 5);
+        Rectangle rectangle = new Rectangle("Blue", 3, 7);
+        Circle circle = new Circle("Yellow", 4);
+
+        List<Shape> shapes = new List<Shape>() { square, rectangle, circle };
+
+        foreach (Shape shape in shapes)
+        {
+            Console.WriteLine(shape.GetColor());
+            Console.WriteLine(shape.GetArea());
+        }
     }
 }
